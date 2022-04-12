b88787c3d035d6a6311dd1503c4f0a7e52ce0215
Salvo94 <salvo1994tpe@gmail.com>
2022-04-12 21:34:02 +0200

    creati i model Cart - Item - con il relativo model intermedio Cart_Item per l'eliminazione parametrica di un singolo Item dal carrello

---
+++ app/Http/Controllers/CartItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart_Item;
+use Illuminate\Http\Request;
+
+class CartItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cart_Item  $cart_Item
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cart_Item $cart_Item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cart_Item  $cart_Item
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cart_Item $cart_Item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cart_Item  $cart_Item
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cart_Item $cart_Item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cart_Item  $cart_Item
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cart_Item $cart_Item)
+    {
+        //
+    }
+}
